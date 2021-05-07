25b46a331be3ff674365baae4f30ba9801adc815
Pavel <pavelbrn@gmail.com>
2021-05-07 16:58:10 +0200

    test and debug aggreate_col_val

--- custom-python-functions/customFunctions.py
+++ custom-python-functions/customFunctions.py
@@ -14,20 +14,19 @@ def change_to_camelcase(string):
     new_string = new_string.strip("_")
     return new_string
 
-# Aggregate a target value inside a dataframe column by its mean values.
+# Aggregate a target value inside a dataframe column to a mean value.
 # tagret_val = Column of values we want aggregated(This is string)
-# int_worth = Column of the corresponding worth(i.e price)(This column head is also a string)
-# the mean worth 
+# int_worth = Column of the corresponding value worth(i.e price)(This column head is also a string)
+# dataframe = DataFrame object created using the pandas library
 def aggreate_col_val(dataframe,column_val: str,column_worth: str):
-    # Returns a series of the top 5 brands, we can acces the specific brand of this series by
-    # referencing its index label.
-    total_val= dataframe[target_val].value_counts()
+
+    total_val= dataframe[column_val].value_counts()
     avg_price = 0
-    brand_mean_dict = {}   
+    mean_dict = {}   
     # Take advantage a vectorization in pandas to compute mean of our target column
-    for brand in total_val.index:
-         avg_price = autos.loc[autos[target_val]==brand,int_worth].mean()#
-         brand_mean_dict[brand]= int(avg_price)
-    return brand_mean_dict
+    for value in total_val.index:
+         avg_price = dataframe.loc[autos[column_val]==value,column_worth].mean()#
+         mean_dict[value]= int(avg_price)
+    return mean_dict
 
 
